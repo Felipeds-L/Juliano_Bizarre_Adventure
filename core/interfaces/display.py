67940a2c0d55aef1ed50e779a04abe18778511ab
marcelohenrique15 <marcelohenriquepessoal15@gmail.com>
2025-03-28 21:30:11 -0300

    feat: Configuração da tela

---
+++ core/interfaces/display.py
@@ -0,0 +1,21 @@
+#################### FUNÇÕES #@#######################
+# -----> tela = Tela() <-----
+#
+# tela.setArea(largura, altura) (int, int) --> Define a largura e a altura da tela em pixels
+# tela.setNome("nome") (str) --> Define o nome da tela do jogo
+
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+class Tela():
+    def __init__(self):
+        self.area = None
+    
+    def setArea(self, largura, altura):
+        pygame.display.set_mode((largura, altura))
+    
+    def setNome(self, nome):
+        pygame.display.set_caption(nome)
