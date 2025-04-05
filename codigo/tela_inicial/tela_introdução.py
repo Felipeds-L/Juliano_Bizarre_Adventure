1f3a50a36ac7481fe961a0615ba5085a74085465
dudarmouras <dudarmouras@gmail.com>
2025-04-05 13:55:42 -0300

    Botão tela inicial consertado

--- codigo/tela_inicial/tela_introdução.py
+++ codigo/tela_inicial/tela_introdução.py
@@ -3,28 +3,42 @@ from configuracoes import *
 
 class TelaInicial:
     def __init__(self, jogo):
-        self.intro_background = pygame.image.load('graficos/tela/introbackground.png').convert_alpha()
-        self.intro_background = pygame.transform.scale(self.intro_background, (JANELA_LARGURA, JANELA_ALTURA))
-
+        self.jogo = jogo
+        self.fundo_intro = pygame.image.load('graficos/tela/introbackground.png').convert_alpha()
+        self.fundo_intro = pygame.transform.scale(self.fundo_intro, (JANELA_LARGURA, JANELA_ALTURA))
         self.titulo = pygame.image.load('codigo/tela_inicial/titulo_jogo.png').convert_alpha()
-        self.titulo_rect = self.titulo.get_rect(center = (JANELA_LARGURA // 2, JANELA_ALTURA // 4))
-
+        self.titulo_rect = self.titulo.get_rect(center=(JANELA_LARGURA // 2, JANELA_ALTURA // 4))
         self.botao_jogar = Botao(JANELA_LARGURA/2, JANELA_ALTURA/2)
-        
-        jogo.display.blit(self.intro_background, (0, 0)) 
-        jogo.display.blit(self.titulo, self.titulo_rect)  
-        jogo.display.blit(self.botao_jogar.image, self.botao_jogar.rect)  
+        self.desenhar()
 
+    def desenhar(self):
+        self.jogo.display.blit(self.fundo_intro, (0, 0))
+        self.jogo.display.blit(self.titulo, self.titulo_rect)
+        self.jogo.display.blit(self.botao_jogar.imagem, self.botao_jogar.rect)
+
+    def verificar_clique(self, evento):
+        if evento.type == pygame.MOUSEBUTTONDOWN:
+            if self.botao_jogar.esta_pressionado(pygame.mouse.get_pos(), pygame.mouse.get_pressed()):
+                self.jogo.estado = 'jogando'
+                self.jogo.tocar_musica()
+
+    def mouse_sobre_botao(self):
+        pos_mouse = pygame.mouse.get_pos()
+        return self.botao_jogar.sobre_pixel(pos_mouse)
 
 class Botao:
-    def __init__(self,x,y):
-        self.image = pygame.image.load('codigo/tela_inicial/botao_jogar.png')
-        self.rect = self.image.get_rect(center = (x, y+100))
-        self.image.blit(self.image, (x, y+100))
-    
+    def __init__(self, x, y):
+        self.imagem = pygame.image.load('codigo/tela_inicial/botao_jogar.png').convert_alpha()
+        self.rect = self.imagem.get_rect(center=(x, y+100))
+        self.mascara = pygame.mask.from_surface(self.imagem)
+
     def esta_pressionado(self, pos, pressionado):
+        if self.sobre_pixel(pos) and pressionado[0]:
+            return True
+        return False
+    
+    def sobre_pixel(self, pos):
+        offset = (pos[0] - self.rect.x, pos[1] - self.rect.y)
         if self.rect.collidepoint(pos):
-            if pressionado[0]:
-                return True
-            return False
+            return self.mascara.get_at(offset) != 0
         return False
